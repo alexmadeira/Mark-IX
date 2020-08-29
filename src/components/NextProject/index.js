bf0a9977924241c09efec4b4f1a7fa125a851aa9
Alex Madeira <alex.c.madeira@gmail.com>
2020-08-29 00:11:46 -0300

    feat: proximo e anterior

--- src/components/NextProject/index.js
+++ src/components/NextProject/index.js
@@ -1,10 +1,28 @@
 import React, { memo } from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { animateScroll as scroll } from 'react-scroll';
 
-import { Container } from './styles';
+import { Container, Link } from './styles';
 
 function NextProject() {
-  return <Container />;
+  const { anterior, proximo } = useSelector(state => state.project.project);
+
+  return (
+    <Container>
+      <Link
+        to={`/projeto/${anterior && anterior.fields.slug}`}
+        onClick={() => scroll.scrollToTop()}
+      >
+        {anterior && anterior.fields.title}
+      </Link>
+      <Link
+        to={`/projeto/${proximo && proximo.fields.slug}`}
+        onClick={() => scroll.scrollToTop()}
+      >
+        {proximo && proximo.fields.title}
+      </Link>
+    </Container>
+  );
 }
 
 export default memo(NextProject);
--- src/components/NextProject/styles.js
+++ src/components/NextProject/styles.js
@@ -1,15 +1,31 @@
 import styled from 'styled-components';
 
-import { Spaces } from '~/styles/Metrics';
+import { Link as RouteLink } from 'react-router-dom';
+
+import { transparentize } from 'polished';
+
+import { Spaces, Colors } from '~/styles/Metrics';
 
 export const Container = styled.div`
   display: grid;
-  width: 1600px;
+  width: 100%;
   max-width: 100%;
-  background: #069;
+
   height: 300px;
   align-self: center;
   margin-bottom: calc(${Spaces.BaseMargin}*10);
-  grid-template-columns: 2;
+  grid-template-columns: 1fr 1fr;
   grid-gap: ${Spaces.BaseMargin};
+`;
+
+export const Link = styled(RouteLink)`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  color: ${Colors.White};
+  background: ${transparentize(0.45, Colors.Black)};
+  font-weight: bold;
+  font-size: 6rem;
 `;
